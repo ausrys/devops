bc9c1cb305057ed4911e040a4e66604afce5f1e9
Ausrys Work <ausrys.mikoliunas@orioninc.com>
2025-07-15 15:09:04 +0300

    first test commit atlantis

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "atlantis-demo-bucket-${random_id.suffix.hex}"
+  force_destroy = true
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
